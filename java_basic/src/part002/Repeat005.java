9c81423eda25827962eaa68e4e58a1b0388124b6
jaewon <wodnjs0610@naver.com>
2025-03-11 21:52:09 +0900

    Class배열 연습

--- java_basic/src/part002/Repeat005.java
+++ java_basic/src/part002/Repeat005.java
@@ -1,32 +1,55 @@
-package part002;
-class Score {
-	private String name;
-	private int kor;
-	private int eng;
-	private int math;
-	private double avg;
-	private String pass;
-	
-}
-
+//package part002;
+//
+//public class Score {
+//    private String name;
+//    private int kor;
+//    private int eng;
+//    private int math;
+//    private double avg;
+//    private String pass;
+//
+//    
+//    public Score(String name, int kor, int eng, int math) {
+//        this.name = name;
+//     this.kor = kor;
+//        this.eng = eng;
+//      this.math = math;
+//      Avg();
+//      Pass();
+//    }
+//
+//    
+//    private void Avg() {
+//      this.avg = (kor + eng + math) / 3.0;
+//   }
+//
+//    
+//    private void Pass() {
+//        this.pass = avg >= 60 ? "합격" : "불합격";
+//    }
+//
+//    // Getter 메서드
+//    public String getName() { return name; }
+//    public int getKor() { return kor; }
+//    public int getEng() { return eng; }
+//    public int getMath() { return math; }
+//    public double getAvg() { return avg; }
+//    public String getPass() { return pass; }
+//}
+//
 //public class Repeat005 {
 //	public static void main(String[] args) {
-//		Score_ver2[] std = new Score[3]; std = {null, null, null} std :3개 객체의 주소묶음
-//		std[0] = new Score("아이언맨",100,100,100);
-//		std[1] = new Score("헐크",90,60,80);
-//		std[2] = new Score("블랙팬서",20,60,90);
 //		
-//		ScoreProcess process = new ScoreProcess();
-//		process.process_avg(std); // std = {101번지 , 102번지 , 103번지} -3개 객체의 주소묶음
-//		process.process.prass(std);
-//		
-//		ScorePrint_ver2 print = new ScorePrint_ver2();
-//		print.show(std);
 //	}
 //}
-//1. Score, ScoreProcess,ScorePrint 클래스와 ClassArr004 클래스
-//패키지를 분리하시오.
-// 클래스들의 옵션은 다음과 같다. 멤버필드를 private 로 설정하시오
-//3. 다음과 같이 출력되도록 main을 채우시오.
-//
-
+////1. Score, ScoreProcess,ScorePrint 클래스와 ClassArr004 클래스
+////패키지를 분리하시오.
+//// 클래스들의 옵션은 다음과 같다. 멤버필드를 private 로 설정하시오
+////3. 다음과 같이 출력되도록 main을 채우시오.
+////
+////::::::::::::::::::::::::::::::::::::::::::::::
+////이름	국어	영어	수학	평균	합격여부
+////::::::::::::::::::::::::::::::::::::::::::::::
+////아이언맨 100 100 100 100.00 합격
+////헐크	90	60	80	76.67	합격
+////블랙팬서 20	60	90	56.67	불합격
