88c2348caa29fe2b82b513cb0ff74c2e4db456a4
Jaewon <wodnjs0610@naver.com>
2025-03-31 18:21:07 +0900

    2025-03-31 Spring

---
+++ javaSpring/.metadata/.plugins/org.eclipse.core.resources/.history/a7/e08ecac60e0e0010199df4ead54c0319
@@ -0,0 +1,11 @@
+package com.company.ioctest2;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public interface Animal {
+	public String eat();
+	public String sleep();
+	public String poo();
+}
---
+++ javaSpring/spring003_bean/src/main/java/com/company/ioctest3/Animal.java
@@ -0,0 +1,11 @@
+package com.company.ioctest3;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public interface Animal {
+	public String eat();
+	public String sleep();
+	public String poo();
+}
